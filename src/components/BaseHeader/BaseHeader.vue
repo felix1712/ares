360a3ede1f020ef9d0a38b48087f5b5872fe3485
felixand <felix.andrean17@gmail.com>
2019-08-08 12:50:07 +0700

    update header

--- src/components/BaseHeader/BaseHeader.vue
+++ src/components/BaseHeader/BaseHeader.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="header">
-		<div class="banner-header">
+	<div class="header" :scroll="handleScroll">
+		<div class="banner-header" :class="{'unpinned': scrolled}" v-if="!showFixedOnly">
 			<div class="logo-banner">
 				<img src="@/assets/images/main-logo.png">
 			</div>
@@ -39,8 +39,46 @@
 				</ul>
 			</div>
 		</div>
-		<div class="header-fix">
-			
+		<div class="header-fix" :class="{'is-show': !scrolled}">
+			<img src="@/assets/images/logo-second.png">
+			<div class="menu-list">
+				<ul>
+					<li>
+						<a class="active">
+							About
+						</a>
+					</li>
+					<li>
+						<a>
+							promo & news
+						</a>
+					</li>
+					<li>
+						<a>
+							property
+						</a>
+					</li>
+					<li>
+						<a>
+							our sales
+						</a>
+					</li>
+					<li>
+						<a>
+							location
+						</a>
+					</li>
+					<li>
+						<a>
+							contact us
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="menu-contact-info">
+				<p>+62 822 1024 4875</p>
+				<small>ares-sarana@hotmail.com</small>
+			</div>
 		</div>
 	</div>
 </template>
@@ -48,6 +86,41 @@
 <script>
 	export default {
 		name: 'base-header',
+		data() {
+			return {
+				limitHeader: 800,
+				lastPosition: 0,
+				scrolled: false,
+			};
+		},
+		methods: {
+			handleScroll() {
+				if (this.lastPosition > this.limitHeader) {
+					this.scrolled = false;
+				} else if (this.lastPosition < this.limitHeader) {
+	      	this.scrolled = true;
+				}
+
+				this.lastPosition = window.scrollY;
+			},
+		},
+		props: {
+			showFixedOnly: {
+				default: false,
+				type: Boolean,
+			}
+		},
+		created() {
+			if(!this.showFixedOnly){
+				this.scrolled = true;
+				window.addEventListener('scroll', this.handleScroll);
+			}
+		},
+		destroyed() {
+			if(!this.showFixedOnly){	
+				window.removeEventListener('scroll', this.handleScroll);
+			}
+		},
 	}
 </script>
 
@@ -56,7 +129,7 @@
 	.header{
 		margin: 0 100px;
 		position: relative;
-		z-index: 2;
+		z-index: 9;
 		ul{
 			padding: 0;
 			margin: 0;
@@ -70,6 +143,11 @@
 			}
 		}
 		.banner-header{
+			&.unpinned{
+				transform: translateY(-100%);
+				transition-timing-function: ease-in;
+		  	transition: 0.2s;
+			}
 			.logo-banner{
 				position: absolute;
 				top: 5vh;
@@ -110,6 +188,88 @@
 						}
 					}
 				}
+			}
+		}
+
+		.header-fix{
+			&.is-show{
+		  	transition: 0.25s;
+			  transition-timing-function: ease-out;
+
+			  transform: translateY(0);
+			  opacity: 1;
+		  }
+
+		  display: flex;
+		  min-height: 50px;
+		  background: rgba(#fff, .7);
+			font-size: 80%;
+			padding: 7px 100px;
+			position: fixed;
+			top: 0;
+			right: 0;
+			width: 100%;
+			z-index: 9;
+			//animation
+			transform: translateY(-100%);
+		  transition-timing-function: ease-in;
+		  transition: 0.2s;
+
+			img{
+				height: auto;
+				width: 75px;
+				max-height: 50px;
+			}
+			.menu-list{
+		    width: auto;
+		    z-index: 2;
+		    -webkit-transition-delay: 1.5s;
+		    transition-delay: 1.5s;
+		    display: flex;
+			  justify-content: center;
+			  flex-direction: column;
+			  margin-left: auto;
+			  margin-right: auto;
+
+				ul{
+					float: right;
+
+					li{
+						display: inline-block;
+						margin-right: 30px;
+						a{
+							color: $v-blue;
+							font-size: 12px;
+							letter-spacing: 1.5px;
+							font-weight: 500;
+							&.active{
+								&:after{
+									content: "";
+									position: absolute;
+									bottom: 0;
+									height: auto;
+									width: 40px;
+									border-bottom: 3px solid $v-second-blue;
+								}
+							}
+						}
+					}
+				}
+			}
+			.menu-contact-info{
+				float: right;
+				width: auto;
+				display: flex;
+				justify-content: center;
+			  flex-direction: column;
+			  p{
+			  	color: $v-blue;
+			  	margin-bottom: 5px;
+			  	font-size: 12px;
+			  }
+			  small{
+			  	color: $v-second-blue;
+			  }
 			}
 		}
 	}
